ca8577d1930f7747ff511ecf6461e6995bc72e0c
Farman71 <farmanhussain71@gmail.com>
2025-01-30 13:54:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
+    let tab=document.getElementById("sampleTable")
     //Write your code here
-  
-  
+    let row=document.createElement("tr")
+    let cell1=document.createElement("td")
+    let cell2=document.createElement("td")
+    cell1.textContent="New Cell1"
+    cell2.textContent="New Cell2"
+    row.appendChild(cell1)
+    row.appendChild(cell2)
+    tab.appendChild(row)
+    tab.insertBefore(row,tab.firstChild)
 }
